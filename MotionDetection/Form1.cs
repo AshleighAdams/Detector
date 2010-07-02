cf975c0a48a6505ebf570dfab8b9adfd0145627b
C0BRA <C0BRA@.(none)>
2010-07-02 14:40:56 +0100

    Motion Blur on demo application

--- MotionDetection/Form1.cs
+++ MotionDetection/Form1.cs
@@ -63,6 +63,7 @@ namespace Detector
 
         private void cbOn_CheckedChanged(object sender, EventArgs e)
         {
+            pbLast.Image = pbCurrent.Image;
             tmrCheckMotion.Enabled = cbOn.Checked;
             detector.IgnoreMotion = pbIgnoreMotion.Image;
         }
@@ -82,38 +83,43 @@ namespace Detector
 
         private void tmrCheckMotion_Tick(object sender, EventArgs e)
         {
+
+            Bitmap blur = new Bitmap(pbLast.Image);
+            Bitmap __cur = new Bitmap(pbCurrent.Image);
+            MotionBlur(ref blur, ref __cur,20);
+            pbLast.Image = blur;
+
             Bitmap cur_img = new Bitmap(pbCurrent.Image);
-
-            if (frames[frames.Length - 1] != null)
+            //if (frames[frames.Length - 1] != null)
+            //{
+            Bitmap bmp = new Bitmap(pbCurrent.Image);
+            detector.SetNextImages(pbLast.Image, pbCurrent.Image);
+            foreach (Target t in detector.GetTargets())
             {
-                Bitmap bmp = new Bitmap(pbCurrent.Image);
-                detector.SetNextImages(pbLast.Image, pbCurrent.Image);
-                foreach (Target t in detector.GetTargets())
+
+                for (int x = t.X; x < t.X + t.SizeX; x++)
                 {
-
-                    for (int x = t.X; x < t.X + t.SizeX; x++)
-                    {
-                        if (x >= cur_img.Width)
-                            break;
-                        bmp.SetPixel(x, t.Y, Color.Red);
-                        bmp.SetPixel(x, Math.Min( cur_img.Height-1, t.Y + t.SizeY), Color.Red);
-                    }
-                    for (int y = t.Y; y < t.Y + t.SizeY; y++)
-                    {
-                        if (y >= cur_img.Height)
-                            break;
-                        bmp.SetPixel(t.X, y, Color.Red);
-                        bmp.SetPixel(Math.Min( cur_img.Width-1, t.X + t.SizeX ), y, Color.Red);
-                    }
-                    
+                    if (x >= cur_img.Width)
+                        break;
+                    bmp.SetPixel(x, t.Y, Color.Red);
+                    bmp.SetPixel(x, Math.Min( cur_img.Height-1, t.Y + t.SizeY), Color.Red);
                 }
-                pbMotion.Image = bmp;
+                for (int y = t.Y; y < t.Y + t.SizeY; y++)
+                {
+                    if (y >= cur_img.Height)
+                        break;
+                    bmp.SetPixel(t.X, y, Color.Red);
+                    bmp.SetPixel(Math.Min( cur_img.Width-1, t.X + t.SizeX ), y, Color.Red);
+                }
+                
             }
-            frames[0] = pbCurrent.Image;
+            pbMotion.Image = bmp;
+            //}
+            //frames[0] = pbCurrent.Image;
 
-            for (int i = frames.Length - 2; i > -1; i--)
-                frames[i + 1] = frames[i];
-            pbLast.Image = frames[frames.Length - 1];
+            //for (int i = frames.Length - 2; i > -1; i--)
+            //    frames[i + 1] = frames[i];
+            //pbLast.Image = frames[frames.Length - 1];
         }
 
         private void tbDifference_Scroll(object sender, EventArgs e)
@@ -126,11 +132,38 @@ namespace Detector
             if (ofd.ShowDialog() == DialogResult.OK)
             {
                 pbIgnoreMotion.ImageLocation = ofd.FileName;
+                System.Threading.Thread.Sleep(50);
                 detector.IgnoreMotion = pbIgnoreMotion.Image;
             }
         }
 
-        
+        private unsafe void MotionBlur(ref Bitmap imagetoblur, ref Bitmap current, int ammount)
+        {
+            BitmapData blur = imagetoblur.LockBits(new Rectangle(0, 0, imagetoblur.Width, imagetoblur.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            BitmapData cur = current.LockBits(new Rectangle(0, 0, current.Width, current.Height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            
+            byte* row_current;
+            byte* row_blur;
+            int R, G, B;
+            for (int y = 0; y < cur.Height; y++)
+                for (int x = 0; x < cur.Width; x++)
+                {
+                    row_current = (byte*)cur.Scan0.ToPointer() + (cur.Stride * y);
+                    row_blur = (byte*)blur.Scan0.ToPointer() + (blur.Stride * y);
+                    B = x * 3 + 0;
+                    G = x * 3 + 1;
+                    R = x * 3 + 2;
+
+                    row_blur[R] = (byte)((int)row_blur[R] + (((int)row_current[R] - (int)row_blur[R]) / ammount));
+                    row_blur[G] = (byte)((int)row_blur[G] + (((int)row_current[G] - (int)row_blur[G]) / ammount));
+                    row_blur[B] = (byte)((int)row_blur[B] + (((int)row_current[B] - (int)row_blur[B]) / ammount));
+
+
+
+                }
+            imagetoblur.UnlockBits(blur);
+            current.UnlockBits(cur);
+        }
 
     }
 }
